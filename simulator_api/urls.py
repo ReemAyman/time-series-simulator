486c0bfb47fa1ca1e21f1446c2367a29c69a46fe
ReemAyman <reemayman5299@gmail.com>
2023-10-01 23:02:14 +0300

    modify: {multi-processing}

--- simulator_api/urls.py
+++ simulator_api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import CreateSimulator, ListSimulator, RunSimulator, StopSimulator
 
 urlpatterns = [
-    path('create', CreateSimulator.as_view(), name="CreateSimulator"),
+    path('', CreateSimulator.as_view(), name="CreateSimulator"),
     path('list', ListSimulator.as_view(), name="ListSimulator"),
     path('stop', StopSimulator.as_view(), name="StopSimulator"),
     path('run', RunSimulator.as_view(), name="RunSimulator")
